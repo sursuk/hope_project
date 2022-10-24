ff917f4e4325cae2319373a94f0d2f55081247f4
Алексей Шемет <shemet162534@gmail.com>
2022-10-24 23:34:56 +0300

    Реализованный список

---
+++ hope_project/singly_linked_list.cpp
@@ -0,0 +1,104 @@
+
+
+#include"singly_linked_list.h";
+
+#include<vector>
+
+
+
+
+
+singly_linked_list::singly_linked_list(std::vector<int> init_arr)
+{
+	for (auto element : init_arr) {
+		push_back(element);
+	}
+}
+
+singly_linked_list::~singly_linked_list()
+{
+}
+
+int singly_linked_list::getValue(int index)
+{
+	if (is_empty()) {
+		throw "list is empty";
+	}
+	if (index > length()) {
+		throw "index > size of list";
+	}
+	Node* node = head;
+	for (int i = 0; i < length(); i++) {
+		node = node->next;
+	}
+	return node->value;
+}
+
+
+void singly_linked_list::putValue(int index, int value)
+{
+	if (index > length()) {
+		throw "index > length of list";
+	}
+
+
+	//put to begin list
+	if (is_empty()) {
+		Node* node = new Node(value);
+		head = node;
+		last = node;
+		size++;
+		return;
+	}
+	if (index == 0) {
+		Node* node = new Node(value);
+		if (head->next == nullptr) {
+			delete head;
+			head = node;
+			return;
+		}
+		else {
+			node->next = head->next;
+			delete head;
+			head = node;
+			return;
+		}
+	}
+	//put to end list
+	if (index == length()) {
+		push_back(value);
+		return;
+	}
+	//put to middle list
+	Node* node = new Node(value);//new element
+
+	auto iterator = head;
+	for (int i = 0; i < index - 1; i++) {
+		iterator = head->next;
+	}
+	node->next = iterator->next->next;
+	delete iterator->next;
+	iterator->next = node;
+}
+
+void singly_linked_list::push_back(int value)
+{
+	Node* node = new Node(value);
+	size++;
+	if (is_empty()) {
+		head = node;
+		return;
+	}
+	last->next = node;
+	last = node;
+}
+
+bool singly_linked_list::is_empty()
+{
+	return head == nullptr;
+}
+
+int singly_linked_list::length()
+{
+	return size;
+}
---
+++ hope_project/singly_linked_list.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include<vector>
+
+class singly_linked_list
+{
+public:
+	singly_linked_list(std::vector<int> init_arr);
+	~singly_linked_list();
+
+	int getValue(int index);
+	void putValue(int index, int value);
+	void push_back(int value);
+
+	bool is_empty();
+	int length();
+
+private:
+	struct Node
+	{
+		int value;
+		Node* next = nullptr; 
+
+		void set_value(int value) { this->value = value; }
+
+		Node(int _value) : value(_value), next(nullptr){}
+	};
+
+	int size = 0;
+	Node* head;
+	Node* last;
+
+};
+
+
